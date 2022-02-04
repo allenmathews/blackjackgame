a53e049be9caf36b04e6cb348c51083f72f4cb9f
Allen Mathews <allenjoemathews@gmail.com>
2022-02-04 17:23:19 +1100

    added array and for loop

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard] // array -ordered list of items
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -9,7 +10,10 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard + " ";
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -31,8 +35,8 @@ function newCard() {
     console.log("Drawing a new card from the deck!")
     let card = 7;
     sum += card;
-
+    cards.push(card)
+    console.log(cards)
     renderGame()
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard + " " + card;
 
 }
